2d8311229cc83abc36dae70ee8d9d516f4481893
Sergey <serbog230797@gmail.com>
2026-03-23 03:22:44 +0300

    упс.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,9 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import MainPage from '../pages/main-page';
+import FavoritesPage from '../pages/favorites-page';
+import OfferPage from '../pages/offer-page';
+import LoginPage from '../pages/login-page';
+import { AppRoute } from '../const';
 
 type AppScreenProps = {
   rentalOffers: number;
@@ -6,7 +11,26 @@ type AppScreenProps = {
 
 function App ({rentalOffers}: AppScreenProps): JSX.Element {
   return (
-    <MainPage rentalOffers = {rentalOffers}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage rentalOffers = {rentalOffers}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage/>}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
